4dd8917b99790374cff507a751a776d110e4a222
brusvega <br.vegav@duocuc.cl>
2025-10-27 09:35:36 -0300

    Resolver conflicto en App.jsx combinando rutas de admin y productos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,26 +7,41 @@ import Contacto from "./pages/Contacto";
 import Nosotros from "./pages/Nosotros";
 import Tienda from "./pages/Tienda";
 import Carrito from "./pages/Carrito";
+
+// 🔹 Rutas Admin (tuyas)
 import AdminLayout from "./pages/admin/AdminLayout";
 import AdminProductos from "./pages/admin/AdminProductos";
+
+// 🔹 Rutas de productos (Paulina)
+import Frutas from "./pages/Frutas";
+import Verduras from "./pages/Verduras";
+import Prodlacteos from "./pages/Prodlacteos";
+import Prodorganicos from "./pages/Prodorganicos";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        {/* Rutas generales */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/tienda" element={<Tienda />} />
         <Route path="/carrito" element={<Carrito />} />
-        {/* RUTAS ADMIN */}
+
+        {/* Rutas Admin */}
         <Route path="/admin" element={<AdminLayout />}>
-        <Route path="productos" element={<AdminProductos />} />
+          <Route path="productos" element={<AdminProductos />} />
         </Route>
+
+        {/* Rutas de productos */}
+        <Route path="/frutas" element={<Frutas />} />
+        <Route path="/verduras" element={<Verduras />} />
+        <Route path="/prod-lacteos" element={<Prodlacteos />} />
+        <Route path="/prod-organicos" element={<Prodorganicos />} />
       </Routes>
-      
 
       <Footer />
     </BrowserRouter>
